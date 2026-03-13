45e0c7ded61ecd287455b0ad14c627fe47a9cfd7
Alex Kahn <43892045+alnoki@users.noreply.github.com>
2026-03-13 15:34:28 -0700

    Line break algo comment

--- src/dropset/entrypoint.s
+++ src/dropset/entrypoint.s
@@ -3,7 +3,8 @@ entrypoint:
     ldxdw r3, [r2 - 8]
     # insn_d = insn.discriminant
     ldxb r4, [r2 + 0]
-    # if insn_d == Discriminant::RegisterMarket return register_market
+    # if insn_d == Discriminant::RegisterMarket \
+    # return register_market
     jeq r4, 0, register_market
     # return Error::InvalidDiscriminant
     mov32 r0, 1
