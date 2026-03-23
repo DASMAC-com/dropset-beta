ecd7d740653acae0a3db9b6fea0cdca43673cd0b
Alex Kahn <43892045+alnoki@users.noreply.github.com>
2026-03-23 15:05:06 -0700

    Fix broken lint/tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,1 @@
 
-pub fn    cache_test(   )    {   }
--- tests/tests/run.rs
+++ tests/tests/run.rs
@@ -3,11 +3,6 @@ mod cases;
 use dropset_tests::{run_and_report, setup};
 
 #[test]
-fn cache_test() {
-    panic!("intentional failure to verify CI caches on failure");
-}
-
-#[test]
 fn all_cases() {
     let setup = setup();
     run_and_report("Entrypoint", cases::entrypoint::Case::ALL, &setup);
