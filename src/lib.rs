8d7734ed7587085e3fbdf8fd6b87b7dfe1c1fdfd
Alex Kahn <43892045+alnoki@users.noreply.github.com>
2026-03-23 15:01:37 -0700

    Trigger fails

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,2 @@
 
+pub fn    cache_test(   )    {   }
--- tests/tests/run.rs
+++ tests/tests/run.rs
@@ -3,6 +3,11 @@ mod cases;
 use dropset_tests::{run_and_report, setup};
 
 #[test]
+fn cache_test() {
+    panic!("intentional failure to verify CI caches on failure");
+}
+
+#[test]
 fn all_cases() {
     let setup = setup();
     run_and_report("Entrypoint", cases::entrypoint::Case::ALL, &setup);
